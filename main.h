93ab7f991d422a1e4f996b1778a5c9d9cb904936
Thomas GRANJON <thomas.granjon@orange.fr>
2017-05-22 09:41:04 +0200

    feat: Test des limites
    NBOBJECTIF : 1 -> 5
    NBOBJET : 20 -> 100

--- main.h
+++ main.h
@@ -8,8 +8,8 @@
 #include "arbre.h"
 #include "modele.h"
 
-#define NBOBJET 20 // Nombre d'objet généré aléatoirement
-#define NBOBJECTIF 1 // Nombre d'objet généré aléatoirement
+#define NBOBJET 100 // Nombre d'objet généré aléatoirement //! Testé jusqu'à 500 -> peu d'espace où se déplacer
+#define NBOBJECTIF 5 // Nombre d'objectifs généré aléatoirement //! Testé jusqu'à 10 -> crash arbre
 
 typedef struct point // Structure Point(x,z)
 {
